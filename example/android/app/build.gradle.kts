3ea1d83fe3d9b6bb7827bab6ba72d3f6565d3863
Nayan <nayanbabariya566@gmail.com>
2025-02-14 17:37:44 +0530

    Upgrade android project to "kts" script

---
+++ example/android/app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.aubergine.signin_with_linkedin_example"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.aubergine.signin_with_linkedin_example"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://docs.flutter.dev/deployment/android#reviewing-the-gradle-build-configuration.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
--- example/android/app/src/main/kotlin/com/aubergine/signin_with_linkedin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/aubergine/signin_with_linkedin_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.example
+package com.aubergine.signin_with_linkedin_example
 
 import io.flutter.embedding.android.FlutterActivity
 
